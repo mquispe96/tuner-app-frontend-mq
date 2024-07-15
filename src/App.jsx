c203a937a7b2d77ca20dd19ca6196445822fe094
mquispe96 <mquispe@pursuit.org>
2024-07-15 11:34:32 -0400

    Created routes and necessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import {Routes, Route, Navigate} from 'react-router-dom';
+import NavBar from './Pages/NavBar';
+import ShowSongs from './Pages/ShowSongs';
+import ShowSong from './Pages/ShowSong';
+import NewSong from './Pages/NewSong';
+import EditSong from './Pages/EditSong';
+import NotFound from './Pages/NotFound';
 
 const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/songs" />} />
+      <Route path="songs" element={<NavBar />}>
+        <Route index element={<ShowSongs />} />
+        <Route path="new" element={<NewSong />} />
+        <Route path=":id" element={<ShowSong />} />
+        <Route path=":id/edit" element={<EditSong />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
-  return ;
-}
-
-export default App
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import './Styling/index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+);
